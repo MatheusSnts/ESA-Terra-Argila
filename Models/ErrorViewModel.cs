55b123fd5e3478096f525bacdfeb3c6f83de6f4a
Matheus Santos <matheus.santos@beltseguros.pt>
2025-01-18 12:42:45 +0000

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ESA_Terra_Argila.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
