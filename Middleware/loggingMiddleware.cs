5d91b758acbf2f37f7c700986f6537f00543d23b
francisco-cunha <202100438@estudantes.ips.pt>
2025-03-10 01:16:41 +0000

    registo de logs commit final

--- Middleware/loggingMiddleware.cs
+++ Middleware/loggingMiddleware.cs
@@ -1,9 +1,11 @@
 ﻿using ESA_Terra_Argila.Data;
 using ESA_Terra_Argila.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace ESA_Terra_Argila.Middleware
 {
@@ -17,22 +19,24 @@ namespace ESA_Terra_Argila.Middleware
             _next = next;
         }
 
-        public async Task Invoke(HttpContext context, ApplicationDbContext dbContext)
+        public async Task Invoke(HttpContext context, ApplicationDbContext dbContext, UserManager<User> userManager)
         {
-           
-                var log = new AccessLog
-                {
+
+            var user = await userManager.GetUserAsync(context.User);
+            var log = new AccessLog
+            {
                 Timestamp = DateTime.UtcNow,
                 IP = context.Connection.RemoteIpAddress?.ToString(),
                 Path = context.Request.Path,
                 Method = context.Request.Method,
-                UserName = "zxzx"
-                };
+                UserName = user?.UserName ?? "Anonymous"
+            };
+            if (log.Method.StartsWith("POST")) { 
             if (log.Path.StartsWith("/Identity/Account/Login") || log.Path.StartsWith("/Identity/Account/Logout"))
             {
                 dbContext.AccessLogs.Add(log);
                 await dbContext.SaveChangesAsync();
-            }
+            } }
                 await _next(context);
             
         }
