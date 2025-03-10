8050ccb7975ebad1d47c27388523ceb9f65f812e
obriann <obrian@datanana.com>
2025-03-10 02:52:19 +0000

    Product/Materials CRUD beta

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -24,41 +24,53 @@ namespace ESA_Terra_Argila.Data
         {
             base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Material>()
+                .HasOne(m => m.Category)
+                .WithMany(c => c.Materials)
+                .HasForeignKey(m => m.CategoryId)
+                .OnDelete(DeleteBehavior.SetNull);
+
             modelBuilder.Entity<ProductMaterial>()
                 .HasOne(pm => pm.Product)
                 .WithMany(p => p.ProductMaterials)
                 .HasForeignKey(pm => pm.ProductId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<ProductMaterial>()
                 .HasOne(pm => pm.Material)
                 .WithMany(m => m.ProductMaterials)
                 .HasForeignKey(pm => pm.MaterialId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<ProductTag>()
                 .HasOne(pt => pt.Product)
                 .WithMany(p => p.ProductTags)
                 .HasForeignKey(pt => pt.ProductId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<ProductTag>()
                 .HasOne(pt => pt.Tag)
                 .WithMany(t => t.ProductTags)
                 .HasForeignKey(pt => pt.TagId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<MaterialTag>()
                 .HasOne(mt => mt.Material)
                 .WithMany(m => m.MaterialTags)
                 .HasForeignKey(mt => mt.MaterialId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<MaterialTag>()
                 .HasOne(mt => mt.Tag)
                 .WithMany(t => t.MaterialTags)
                 .HasForeignKey(mt => mt.TagId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
