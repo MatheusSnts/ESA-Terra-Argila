879545d026c8510b6bbab8844310c908c9ee1ac6
202200030 <127192259+202200030@users.noreply.github.com>
2025-03-30 15:20:20 +0100

    EnumType

---
+++ ESA-Terra-Argila/Enums/UnitType.cs
@@ -0,0 +1,27 @@
+﻿namespace ESA_Terra_Argila.Enums
+{
+    //Enum for the type of unit of a product or material (e.g. gram, kilogram, liter, etc.)
+    //If more nedded, add more values to the enum and update the database 
+    public enum UnitType
+        {
+            None = 0,
+            Gram = 1,
+            Kilogram = 2,
+            Liter = 3,
+            Milliliter = 4,
+            Piece = 5,
+            Meter = 6,
+            SquareMeter = 7,
+            CubicMeter = 8,
+            Ton = 9,
+            Millimeter = 10,
+            Centimeter = 11,
+            Kilometer = 12,
+            Inch = 13,
+            Ounce = 17,
+            Pound = 18,
+            Gallon = 19,
+            Milligram = 26,
+            Centigram = 27
+    }
+}
